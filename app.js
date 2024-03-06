0437f34766f20c33605e00e28411b486e3fadc90
Vandana Ganwal <vandanaganwal12@gmail.com>
2024-03-06 11:31:11 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const mainImage = document.getElementById("headerImage");
+const heading = document.getElementById("heading");
+const para = document.getElementById("para");
+
+const back = document.getElementById("back");
+const next = document.getElementById("next");
+
+
